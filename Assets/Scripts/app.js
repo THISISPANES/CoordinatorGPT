072fa96c054fab39394d113351a4205ee1eb58d9
THISISPANES <shimon.arai@outlook.com>
2024-05-28 11:19:47 -0700

    Changed index.html and app.js

--- Assets/Scripts/app.js
+++ Assets/Scripts/app.js
@@ -37,7 +37,7 @@ app.use('/api', apiRouter);
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Protect the restricted route
