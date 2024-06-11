61a147ba406315e94186e323daad6c0c95c92b0e
THISISPANES <shimon.arai@outlook.com>
2024-06-11 00:19:08 -0700

    Changed page2.html

--- public/Assets/Scripts/app.js
+++ public/Assets/Scripts/app.js
@@ -39,6 +39,10 @@ app.get('/page2', checkAuth, (req, res) => {
     const user = req.session.user;
 });
 
+apiRouter.get('/user', (req, res) => {
+    res.json(req.session.user);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
