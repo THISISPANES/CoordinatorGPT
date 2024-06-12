d1d6b771334db1b6d2291b9d07944acf77571c7e
THISISPANES <shimon.arai@outlook.com>
2024-06-12 00:16:04 -0700

    Changed a lot

--- server/api.js
+++ server/api.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { google } = require("googleapis");
 const router = express.Router();
+const { OAuth2Client } = require('google-auth-library');
+const CLIENT_ID = '632513860763-fd7ofvc6ul9ucr5sh3ai8uak7c9jqa6u.apps.googleusercontent.com';
+const client = new OAuth2Client(CLIENT_ID);
 
 async function getCourses(accessToken) {
   const classroom = google.classroom({ version: "v1", auth: accessToken });
@@ -30,6 +33,21 @@ router.get('/user', (req, res) => {
   } else {
     res.status(401).json({ message: 'Not authenticated' });
   }
+});
+
+router.post('/login', async (req, res) => {
+  const { id_token } = req.body;
+  const ticket = await client.verifyIdToken({
+    idToken: id_token,
+    audience: CLIENT_ID,
+  });
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+
+  // Create a session for the user
+  req.session.user = userid;
+
+  res.json({ message: 'Logged in' });
 });
 
 module.exports = router;
--- server/app.js
+++ server/app.js
@@ -8,9 +8,9 @@ const authRouter = require('./auth'); // Adjusted to require directly from the s
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(session({ secret: 'your-session-secret', resave: false, saveUninitialized: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public'))); // Adjusted to point to the public folder at the same level as the server folder
-
 app.use(session({
     secret: '."W90N.{tM$+8k&jf7J.+vb)T@wu+f3n6]k4=&q',
     resave: false,
@@ -28,7 +28,7 @@ function isAuthenticated(req, res, next) {
 
 app.use('/auth', authRouter);
 
-app.use('/api', isAuthenticated, apiRouter);
+app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html')); // Adjusted to point to the index.html file in the public folder
@@ -38,6 +38,7 @@ app.get('/page2', isAuthenticated, (req, res) => { // Replaced checkAuth with is
     const user = req.session.user;
     // You might want to send a response here, like res.sendFile() or res.json()
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
